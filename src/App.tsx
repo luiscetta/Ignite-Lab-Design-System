021ed1d7b98a56d7b8807c307cd627ffddca74d6
Luis Cetta <filipe.cetta@gmail.com>
2022-10-27 16:35:43 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './styles/global.css';
+
+
+export function App() {
+  return (
+    <>
+      <h1 className='font-bold text-2xl text-violet-500'>Hello cambada!</h1>
+
+      <button className="bg-cyan-500 font-semibold px-4 py-2 rounded text-gray-900 hover:bg-cyan-300">
+        Enviar
+      </button>
+    </>
+  )
+} 
